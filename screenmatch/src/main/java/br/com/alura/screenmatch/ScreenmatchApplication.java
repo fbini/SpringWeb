0f23be06d9867399d55ba44a8b0deb9d3542c27d
Ingrid Bini <binifidelis@gmail.com>
2025-10-08 11:51:54 -0300

    Conceitos iniciais Spring

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,11 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.service.ConsumoApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static java.lang.System.out;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -13,6 +16,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Primeiro projeto Spring web");
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?i=tt3896198&apikey=9025175");
+		out.print(json);
+
 	}
 }
---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,19 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+//alias - é como um apelido, serve para procurar pelo nome "Title" que está na api, mas o nome
+// vai pelo o que está na váriavel que definimos
+
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao){
+                         //property - serve para procurar e escrever o nome ex- imdbVotes e procura imdVotes
+                         // @JsonProperty("imdbVotes") String votos) {
+
+
+
+}
+
+
